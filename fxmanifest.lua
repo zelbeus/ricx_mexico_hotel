990d79642a415254d111031def8f1631cb5f6dbf
ricx <66686454+zelbeus@users.noreply.github.com>
2022-06-11 11:48:52 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "cerulean"
 game "rdr3"
 rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
-
+lua54 "yes"
 this_is_a_map "yes"
 
 escrow_ignore {
